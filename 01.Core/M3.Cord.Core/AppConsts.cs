ae34aaba9f638744be5ae812ff32bc15bd035d09
chumpon-asaneerat <chumponsenate@gmail.com>
2023-03-09 09:15:52 +0700

    M3 Cord Production System #6 - Add M3.Cord.Production.App project

---
+++ 01.Core/M3.Cord.Core/AppConsts.cs
@@ -0,0 +1,42 @@
+﻿#region Using
+
+using System;
+
+#endregion
+
+namespace M3.Cord
+{
+    public static class AppConsts
+    {
+        // common properties
+        public static string Version = "1";
+        public static string Minor = "0";
+        public static string CompanyName = "VSEG";
+
+        public static class Application
+        {
+            public static class Cord
+            {
+                public static class Production
+                {
+                    public static string ApplicationName = @"M3 Cord Production Application";
+                    // common
+                    public static string Version = AppConsts.Version;
+                    public static string Minor = AppConsts.Minor;
+                    public static string Build = "1";
+                    public static DateTime LastUpdate = new DateTime(2023, 3, 9, 23, 20, 00);
+                }
+
+                public static class QA
+                {
+                    public static string ApplicationName = @"M3 Cord QA Application";
+                    // common
+                    public static string Version = AppConsts.Version;
+                    public static string Minor = AppConsts.Minor;
+                    public static string Build = "1";
+                    public static DateTime LastUpdate = new DateTime(2023, 3, 9, 23, 20, 00);
+                }
+            }
+        }
+    }
+}
