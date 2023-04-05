7fbd69d6d2e9fd4e929d82f237305d9bd663b796
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-05 22:23:51 +0700

    M3 Cord Production System #61 - Add WpfSendKeys project

---
+++ 20.Tests/WpfSendKeys/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace WpfSendKeys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdSend_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = txtTextToSend.Text.Trim();
+
+            KeyConverter converter = new KeyConverter();
+            for (int i = 0; i < txt.Length; i++) 
+            {
+                string chr = txt[i].ToString();
+                Key key = (Key)converter.ConvertFromString(chr);
+                SendKeys.Send(key);
+            }
+        }
+
+        #endregion
+    }
+
+    public static class SendKeys
+    {
+        /// <summary>
+        ///   Sends the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        public static void Send(Key key)
+        {
+            if (Keyboard.PrimaryDevice != null)
+            {
+                if (Keyboard.PrimaryDevice.ActiveSource != null)
+                {
+                    var e1 = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key) { RoutedEvent = Keyboard.KeyDownEvent };
+                    InputManager.Current.ProcessInput(e1);
+                }
+            }
+        }
+    }
+}
