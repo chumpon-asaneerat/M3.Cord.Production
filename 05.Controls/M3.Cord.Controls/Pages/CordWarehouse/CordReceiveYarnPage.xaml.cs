bbb27f43928c1fef8a0eda6cc6432d69012ad14a
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-11 05:41:00 +0700

    M3 Cord Production System #98 - M3.Cord.Controls project   - Implements UI #27.

---
+++ 05.Controls/M3.Cord.Controls/Pages/CordWarehouse/CordReceiveYarnPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿#region Using
+
+using M3.Cord.Models;
+using NLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for CordReceiveYarnPage.xaml
+    /// </summary>
+    public partial class CordReceiveYarnPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordReceiveYarnPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            GotoMainMenu();
+        }
+
+        private void cmdSearch_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshGrid();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GotoMainMenu()
+        {
+            // Init Main Menu
+            var page = M3CordApp.Pages.CordMainMenu;
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void RefreshGrid()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            //LoadComboBoxes();
+            RefreshGrid();
+        }
+
+        #endregion
+    }
+}
