45abb7d1edff00f0862f6e4f2b36327a7f4f1c54
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-16 01:00:56 +0700

    M3 Cord Production System #107 - Update WpfOracleConnect project #2.   - Implements test code #2.

--- 20.Tests/WpfOracleConnect/MainWindow.xaml.cs
+++ 20.Tests/WpfOracleConnect/MainWindow.xaml.cs
@@ -63,7 +63,21 @@ namespace WpfOracleConnect
 
         private void Connect()
         {
+            /*
+// create connection
+OracleConnection con = new OracleConnection();
 
+// create connection string using builder
+OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+ocsb.Password = "autumn117";
+ocsb.UserID = "john";
+ocsb.DataSource = "database.url:port/databasename";
+
+// connect
+con.ConnectionString = ocsb.ConnectionString;
+con.Open();
+Console.WriteLine("Connection established (" + con.ServerVersion + ")");
+            */
         }
 
         private void Disconnect()
