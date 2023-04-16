6bc839dbe689485096ff07b10d5c357ff0c6d34e
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-16 22:13:03 +0700

    M3 Cord Production System #125 - Add NLib.Wpf.Test.App project

---
+++ 20.Tests/NLib.Wpf.Test.App/App.xaml.cs
@@ -0,0 +1,144 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+
+using NLib;
+using NLib.Logs;
+
+#endregion
+
+namespace NLib.Wpf.Test.App
+{
+    public class AppConsts
+    {
+        // common properties
+        public static string Version = "1";
+        public static string Minor = "0";
+        public static string CompanyName = "Softbase Co., Ltd.";
+
+        public static class Application
+        {
+            public static class NLibApp
+            {
+                public static string ApplicationName = @"NLib Test Application";
+                // common
+                public static string Version = AppConsts.Version;
+                public static string Minor = AppConsts.Minor;
+                public static string Build = "1";
+                public static DateTime LastUpdate = new DateTime(2023, 4, 16, 22, 20, 00);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// OnStartup.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            #region Check Current Domain/Context
+
+            Console.WriteLine("OnStartUp");
+            if (null != AppDomain.CurrentDomain)
+            {
+                if (null != System.Threading.Thread.CurrentContext)
+                {
+                    Console.WriteLine("Thread CurrentContext is not null.");
+                }
+            }
+
+            #endregion
+
+            #region Create Application Environment Options
+
+            EnvironmentOptions option = new EnvironmentOptions()
+            {
+                /* Setup Application Information */
+                AppInfo = new NAppInformation()
+                {
+                    /*  This property is required */
+                    CompanyName = AppConsts.CompanyName,
+                    /*  This property is required */
+                    ProductName = AppConsts.Application.NLibApp.ApplicationName,
+                    /* For Application Version */
+                    Version = AppConsts.Application.NLibApp.Version,
+                    Minor = AppConsts.Application.NLibApp.Minor,
+                    Build = AppConsts.Application.NLibApp.Build,
+                    LastUpdate = AppConsts.Application.NLibApp.LastUpdate
+                },
+                /* Setup Storage */
+                Storage = new NAppStorage()
+                {
+                    StorageType = NAppFolder.Application
+                },
+                /* Setup Behaviors */
+                Behaviors = new NAppBehaviors()
+                {
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = true,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+                }
+            };
+
+            #endregion
+
+            #region Setup Option to Controller and check instance
+
+            WpfAppContoller.Instance.Setup(option);
+
+            if (option.Behaviors.IsSingleAppInstance &&
+                WpfAppContoller.Instance.HasMoreInstance)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Init Preload classes
+
+            ApplicationManager.Instance.Preload(() =>
+            {
+
+            });
+
+            #endregion
+
+            // Start log manager
+            LogManager.Instance.Start();
+
+            Window window = null;
+            window = new MainWindow();
+
+            if (null != window)
+            {
+                WpfAppContoller.Instance.Run(window);
+            }
+        }
+        /// <summary>
+        /// OnExit
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Shutdown log manager
+            LogManager.Instance.Shutdown();
+
+            // Wpf shutdown process required exit code.
+
+            /* If auto close the single instance must be true */
+            bool autoCloseProcess = true;
+            WpfAppContoller.Instance.Shutdown(autoCloseProcess, e.ApplicationExitCode);
+
+            base.OnExit(e);
+        }
+    }
+}
