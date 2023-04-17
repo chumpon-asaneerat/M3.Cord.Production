60ea306005fb7a50290889d13621085539a2d122
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-18 06:33:17 +0700

    M3 Cord Production System #158 - NLib.Wpf.Controls project Implements #25.   - NPage control implements #1.

---
+++ 00.NLib/NLib.Wpf.Controls/Pages/NPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace NLib.Wpf.Pages
+{
+    /// <summary>
+    /// Interaction logic for NPage.xaml
+    /// </summary>
+    public partial class NPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #region PageTitle
+
+        /// <summary>
+        /// The PageTitleProperty dependency property.
+        /// </summary>
+        public static readonly DependencyProperty PageTitleProperty =
+            DependencyProperty.Register("PageTitle", typeof(string), typeof(NPage));
+        /// <summary>
+        /// Gets or sets Page Title.
+        /// </summary>
+        public string PageTitle
+        {
+            get { return (string)GetValue(PageTitleProperty); }
+            set { SetValue(PageTitleProperty, value); }
+        }
+
+        #endregion
+
+        #region WorkArea
+
+        /// <summary>
+        /// The WorkAreaProperty dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WorkAreaProperty =
+            DependencyProperty.Register("WorkArea", typeof(object), typeof(NPage));
+        /// <summary>
+        /// Gets or sets WorkArea Content.
+        /// </summary>
+        public object WorkArea
+        {
+            get { return (object)GetValue(WorkAreaProperty); }
+            set { SetValue(WorkAreaProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public Events
+
+        #endregion
+    }
+}
