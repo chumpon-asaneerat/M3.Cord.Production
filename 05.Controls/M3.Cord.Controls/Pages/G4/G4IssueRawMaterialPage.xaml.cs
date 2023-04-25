ea1b4e63f5c61af426db95aeb4e13f7657eabe5c
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-26 06:50:33 +0700

    M3 Cord Production System #195 - M3.Cord.Production.App re-implements #5.

---
+++ 05.Controls/M3.Cord.Controls/Pages/G4/G4IssueRawMaterialPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for G4IssueRawMaterialPage.xaml
+    /// </summary>
+    public partial class G4IssueRawMaterialPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public G4IssueRawMaterialPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
