eeb0a2d4a9ec8586d6ba9131ab249e4c6b070806
chumpon-asaneerat <chumponsenate@gmail.com>
2023-04-25 15:45:51 +0700

    M3 Cord Production System #181 - Add M3.Cord.Developer.Tools.App project.

---
+++ M3.Cord.Developer.Tools.App/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+
+//using NLib;
+//using NLib.Logs;
+
+#endregion
+
+namespace M3.Cord
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
