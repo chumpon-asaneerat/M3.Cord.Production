2aa8886efe33d0c339c47192332f02f34314e14f
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-16 21:51:42 +0700

    M3 Cord Production System #213 - M3.Cord.Production.App re-implements #20.

---
+++ 00.NLib/NLib.Wpf.Controls/Controls/PageNavigationButton.xaml.cs
@@ -0,0 +1,121 @@
+﻿#region Using
+
+using NLib.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace NLib.Controls
+{
+    /// <summary>
+    /// Interaction logic for PageNavigationButton.xaml
+    /// </summary>
+    public partial class PageNavigationButton : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PageNavigationButton()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #region Dispatcher helper method (invoke event)
+
+        private void InvokeAction(Action action)
+        {
+            try
+            {
+                if (null == action) return;
+                if (null != Application.Current.Dispatcher)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(action);
+                }
+                else
+                {
+                    action();
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmd_Click(object sender, RoutedEventArgs e)
+        {
+            if (null != Click)
+            {
+                InvokeAction(new Action(() =>
+                {
+                    e.Source = this; // Change source.
+                    Click(this, e);
+                }));
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public Properties
+
+        #region IconType
+
+        /// <summary>
+        /// The IconTypeProperty Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty IconTypeProperty =
+            DependencyProperty.Register("IconType", typeof(PageNavigationEnum), typeof(PageNavigationButton));
+        /// <summary>
+        /// Gets or sets Inline Button Icon.
+        /// </summary>
+        public PageNavigationEnum IconType
+        {
+            get { return (PageNavigationEnum)GetValue(IconTypeProperty); }
+            set { SetValue(IconTypeProperty, value); }
+        }
+
+        #endregion
+
+        #region Text
+
+        /// <summary>
+        /// The TextProperty Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(PageNavigationButton));
+        /// <summary>
+        /// Gets or sets Inline Button Text.
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// The Click Event.
+        /// </summary>
+        public event RoutedEventHandler Click;
+
+        #endregion
+    }
+}
---
+++ 00.NLib/NLib.Wpf.Controls/Controls/RecordOperationButton.xaml.cs
@@ -0,0 +1,121 @@
+﻿#region Using
+
+using NLib.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+#endregion
+
+namespace NLib.Controls
+{
+    /// <summary>
+    /// Interaction logic for RecordOperationButton.xaml
+    /// </summary>
+    public partial class RecordOperationButton : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public RecordOperationButton()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #region Dispatcher helper method (invoke event)
+
+        private void InvokeAction(Action action)
+        {
+            try
+            {
+                if (null == action) return;
+                if (null != Application.Current.Dispatcher)
+                {
+                    Application.Current.Dispatcher.BeginInvoke(action);
+                }
+                else
+                {
+                    action();
+                }
+            }
+            catch { }
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmd_Click(object sender, RoutedEventArgs e)
+        {
+            if (null != Click)
+            {
+                InvokeAction(new Action(() =>
+                {
+                    e.Source = this; // Change source.
+                    Click(this, e);
+                }));
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public Properties
+
+        #region IconType
+
+        /// <summary>
+        /// The IconTypeProperty Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty IconTypeProperty =
+            DependencyProperty.Register("IconType", typeof(RecordOperationEnum), typeof(RecordOperationButton));
+        /// <summary>
+        /// Gets or sets Inline Button Icon.
+        /// </summary>
+        public RecordOperationEnum IconType
+        {
+            get { return (RecordOperationEnum)GetValue(IconTypeProperty); }
+            set { SetValue(IconTypeProperty, value); }
+        }
+
+        #endregion
+
+        #region Text
+
+        /// <summary>
+        /// The TextProperty Dependency Property.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(RecordOperationButton));
+        /// <summary>
+        /// Gets or sets Inline Button Text.
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// The Click Event.
+        /// </summary>
+        public event RoutedEventHandler Click;
+
+        #endregion
+    }
+}
