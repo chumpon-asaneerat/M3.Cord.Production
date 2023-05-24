156c74edbe792269743e538cc5b9782afbaab71d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-24 08:01:12 +0700

    M3 Cord Production System #253 - M3.Cord.Production.App re-implements #56.

--- 05.Controls/M3.Cord.Controls/Controls/Fields/ProductionConditions/ProductionConditionL01.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Controls/Fields/ProductionConditions/ProductionConditionL01.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+#endregion
+
 namespace M3.Cord.Controls.Fields
 {
     /// <summary>
@@ -20,9 +24,16 @@ namespace M3.Cord.Controls.Fields
     /// </summary>
     public partial class ProductionConditionL01 : UserControl
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public ProductionConditionL01()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
