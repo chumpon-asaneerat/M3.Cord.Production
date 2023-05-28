e4ea71688a0db227c8c148e50fff0d5b8578714d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-28 17:43:39 +0700

    M3 Cord Production System #275 - M3.Cord.Production.App re-implements #76.

---
+++ 00.NLib/NLib.Wpf.Controls/Pages/NGroupMenu/NGroupMenuItem.cs
@@ -0,0 +1,55 @@
+﻿#region Using
+
+using NLib.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace NLib.Wpf.Pages
+{
+    /// <summary>
+    /// The NGroupMenuItem Control
+    /// </summary>
+    public class NGroupMenuItem : Control
+    {
+        #region Constructor
+
+        static NGroupMenuItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NGroupMenuItem), new FrameworkPropertyMetadata(typeof(NGroupMenuItem)));
+        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NGroupMenuItem()
+        {
+
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        #endregion
+    }
+}
