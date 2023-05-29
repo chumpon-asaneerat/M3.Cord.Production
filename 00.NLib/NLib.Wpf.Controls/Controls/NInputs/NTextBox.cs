5e32186e90800f0c8db796add6023608a62ca936
chumpon-asaneerat <chumponsenate@gmail.com>
2023-05-29 23:30:01 +0700

    M3 Cord Production System #293 - M3.Cord.Production.App re-implements #93.

--- 00.NLib/NLib.Wpf.Controls/Controls/NInputs/NTextBox.cs
+++ 00.NLib/NLib.Wpf.Controls/Controls/NInputs/NTextBox.cs
@@ -53,5 +53,99 @@ namespace NLib.Wpf.Controls
         }
 
         #endregion
+
+        #region Public Properties
+
+        #region Text
+
+        /// <summary>
+        /// The TextProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(NTextBox));
+        /// <summary>
+        /// Gets or sets Text.
+        /// </summary>
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        #endregion
+
+        #region TextAlignment
+
+        /// <summary>
+        /// The TextAlignmentProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TextAlignmentProperty =
+            DependencyProperty.Register("TextAlignment", typeof(TextAlignment), typeof(NTextBox));
+        /// <summary>
+        /// Gets or sets TextAlignment.
+        /// </summary>
+        public TextAlignment TextAlignment
+        {
+            get { return (TextAlignment)GetValue(TextAlignmentProperty); }
+            set { SetValue(TextAlignmentProperty, value); }
+        }
+
+        #endregion
+
+        #region TextAlignment
+
+        /// <summary>
+        /// The TextAlignmentProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty TextWrappingProperty =
+            DependencyProperty.Register("TextWrapping", typeof(TextWrapping), typeof(NTextBox));
+        /// <summary>
+        /// Gets or sets TextWrapping.
+        /// </summary>
+        public TextWrapping TextWrapping
+        {
+            get { return (TextWrapping)GetValue(TextWrappingProperty); }
+            set { SetValue(TextWrappingProperty, value); }
+        }
+
+        #endregion
+
+        #region AcceptsReturn
+
+        /// <summary>
+        /// The AcceptsReturnProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty AcceptsReturnProperty =
+            DependencyProperty.Register("AcceptsReturn", typeof(bool), typeof(NTextBox));
+        /// <summary>
+        /// Gets or sets AcceptsReturn.
+        /// </summary>
+        public bool AcceptsReturn
+        {
+            get { return (bool)GetValue(AcceptsReturnProperty); }
+            set { SetValue(AcceptsReturnProperty, value); }
+        }
+
+        #endregion
+
+        #region AcceptsTab
+
+        /// <summary>
+        /// The AcceptsTabProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty AcceptsTabProperty =
+            DependencyProperty.Register("AcceptsTab", typeof(bool), typeof(NTextBox));
+        /// <summary>
+        /// Gets or sets AcceptsTab.
+        /// </summary>
+        public bool AcceptsTab
+        {
+            get { return (bool)GetValue(AcceptsTabProperty); }
+            set { SetValue(AcceptsTabProperty, value); }
+        }
+
+        #endregion
+
+        #endregion
     }
 }
