fe4420f24ea3be4e0603ecaaab549f7a71addd67
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-01 16:29:54 +0700

    M3 Cord Production System #312 - M3.Cord.Production.App re-implements #112.

--- 00.NLib/NLib.Wpf.Controls/Controls/NInputs/NTextBox.cs
+++ 00.NLib/NLib.Wpf.Controls/Controls/NInputs/NTextBox.cs
@@ -25,6 +25,11 @@ using NLib;
 namespace NLib.Wpf.Controls
 {
     /// <summary>
+    /// The Watermark Image Alignment Enum
+    /// </summary>
+    public enum WatermarkImageAlignment { Left, Right }
+
+    /// <summary>
     /// The NTextBox Control
     /// </summary>
     public class NTextBox : NInputControlBase
@@ -206,6 +211,182 @@ namespace NLib.Wpf.Controls
         {
             get { return (bool)GetValue(IsReadOnlyProperty); }
             set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Image Source
+
+        /// <summary>
+        /// The WatermarkImageSourceProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkImageSourceProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkImageSource),
+                typeof(ImageSource),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Image Source.
+        /// </summary>
+        public ImageSource WatermarkImageSource
+        {
+            get { return (ImageSource)GetValue(WatermarkImageSourceProperty); }
+            set { SetValue(WatermarkImageSourceProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Width
+
+        /// <summary>
+        /// The WatermarkImageWidthProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkImageWidthProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkImageWidth),
+                typeof(double),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata((double)25, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Image Width.
+        /// </summary>
+        public double WatermarkImageWidth
+        {
+            get { return (double)GetValue(WatermarkImageWidthProperty); }
+            set { SetValue(WatermarkImageWidthProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Height
+
+        /// <summary>
+        /// The WatermarkImageHeightProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkImageHeightProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkImageHeight),
+                typeof(double),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata((double)25, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Image Height.
+        /// </summary>
+        public double WatermarkImageHeight
+        {
+            get { return (double)GetValue(WatermarkImageHeightProperty); }
+            set { SetValue(WatermarkImageHeightProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Image Margin
+
+        /// <summary>
+        /// The WatermarkMarginProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkMarginProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkMargin),
+                typeof(Thickness),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata(new Thickness(10, 1, 10, 1), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Image Margin.
+        /// </summary>
+        public Thickness WatermarkMargin
+        {
+            get { return (Thickness)GetValue(WatermarkMarginProperty); }
+            set { SetValue(WatermarkMarginProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Image Alignment
+
+        /// <summary>
+        /// The WatermarkImageAlignmentProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkImageAlignmentProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkImageAlignment),
+                typeof(WatermarkImageAlignment),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata(WatermarkImageAlignment.Left, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Foreground.
+        /// </summary>
+        public WatermarkImageAlignment WatermarkImageAlignment
+        {
+            get { return (WatermarkImageAlignment)GetValue(WatermarkImageAlignmentProperty); }
+            set { SetValue(WatermarkImageAlignmentProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Foreground
+
+        /// <summary>
+        /// The WatermarkForegroundProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkForegroundProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkForeground),
+                typeof(Brush),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata(Brushes.DimGray, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Foreground.
+        /// </summary>
+        public Brush WatermarkForeground
+        {
+            get { return (Brush)GetValue(WatermarkForegroundProperty); }
+            set { SetValue(WatermarkForegroundProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Margin
+
+        /// <summary>
+        /// The WatermarkImageMarginProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkImageMarginProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkImageMargin),
+                typeof(Thickness),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata(new Thickness(5, 1, 5, 1), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Image Margin.
+        /// </summary>
+        public Thickness WatermarkImageMargin
+        {
+            get { return (Thickness)GetValue(WatermarkImageMarginProperty); }
+            set { SetValue(WatermarkImageMarginProperty, value); }
+        }
+
+        #endregion
+
+        #region Watermark Text
+
+        /// <summary>
+        /// The WatermarkTextProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.Register(
+                nameof(WatermarkText),
+                typeof(string),
+                typeof(NTextBox),
+                new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Watermark Text.
+        /// </summary>
+        public string WatermarkText
+        {
+            get { return (string)GetValue(WatermarkTextProperty); }
+            set { SetValue(WatermarkTextProperty, value); }
         }
 
         #endregion
