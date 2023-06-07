eeecd6b0ad0b777319fa6988498c9d6205f78305
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-08 03:19:47 +0700

    M3 Cord Production System #344 - M3.Cord.Production.App re-implements #144.

---
+++ 05.Controls/M3.Cord.Controls/Pages/Warehouse/CordProductPlanningPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for CordProductPlanningPage.xaml
+    /// </summary>
+    public partial class CordProductPlanningPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordProductPlanningPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            M3CordApp.Pages.GotoCordMainMenu();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ResetControls()
+        {
+
+        }
+
+        private void RefreshGrid()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            ResetControls();
+            RefreshGrid();
+        }
+
+        #endregion
+    }
+}
