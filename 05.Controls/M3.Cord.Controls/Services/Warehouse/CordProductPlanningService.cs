cf678894734ebe5191ae7d71e3072bd915e5f392
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-08 03:31:00 +0700

    M3 Cord Production System #347 - M3.Cord.Production.App re-implements #147.

---
+++ 05.Controls/M3.Cord.Controls/Services/Warehouse/CordProductPlanningService.cs
@@ -0,0 +1,66 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+using NLib;
+using NLib.Models;
+using M3.Cord.Models;
+
+#endregion
+
+namespace M3.Cord
+{
+    /// <summary>
+    /// The Cord Product Planning Service.
+    /// </summary>
+    public class CordProductPlanningService : NSingelton<CordProductPlanningService>
+    {
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected CordProductPlanningService() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~CordProductPlanningService()
+        {
+
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private int _totalPallet = 0;
+        private decimal _totalWeight = decimal.Zero;
+        private decimal _totalCH = decimal.Zero;
+
+        #endregion
+
+        #region Privet Methods
+
+        private void CalcTotals()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets Total Pallet.</summary>
+        public int TotalPallet { get { return _totalPallet; } set { } }
+        /// <summary>Gets Total Weight.</summary>
+        public decimal TotalWeight { get { return _totalWeight; } set { } }
+        /// <summary>Gets Total CH (Cheese).</summary>
+        public decimal TotalCH { get { return _totalCH; } set { } }
+
+        #endregion
+    }
+}
---
+++ 05.Controls/M3.Cord.Controls/Services/Warehouse/WarehouseReceiveYarnService.cs
@@ -0,0 +1,66 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+
+using NLib;
+using NLib.Models;
+using M3.Cord.Models;
+
+#endregion
+
+namespace M3.Cord
+{
+    /// <summary>
+    /// The Warehouse Reveive Yarn Service.
+    /// </summary>
+    public class WarehouseReceiveYarnService : NSingelton<WarehouseReceiveYarnService>
+    {
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        protected WarehouseReceiveYarnService() : base()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~WarehouseReceiveYarnService()
+        {
+
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private int _totalPallet = 0;
+        private decimal _totalWeight = decimal.Zero;
+        private decimal _totalCH = decimal.Zero;
+
+        #endregion
+
+        #region Privet Methods
+
+        private void CalcTotals()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>Gets Total Pallet.</summary>
+        public int TotalPallet { get { return _totalPallet; } set { } }
+        /// <summary>Gets Total Weight.</summary>
+        public decimal TotalWeight { get { return _totalWeight; } set { } }
+        /// <summary>Gets Total CH (Cheese).</summary>
+        public decimal TotalCH { get { return _totalCH; } set { } }
+
+        #endregion
+    }
+}
