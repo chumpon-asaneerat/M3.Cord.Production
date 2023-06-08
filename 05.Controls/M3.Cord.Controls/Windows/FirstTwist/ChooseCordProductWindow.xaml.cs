e888ea19481e0929f1e8f0f9a2783df934d67e26
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-08 10:09:36 +0700

    M3 Cord Production System #357 - M3.Cord.Production.App re-implements #157.

---
+++ 05.Controls/M3.Cord.Controls/Windows/FirstTwist/ChooseCordProductWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using NLib.Models;
+using M3.Cord.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Windows
+{
+    /// <summary>
+    /// Interaction logic for CordProcuctPlanningWindow.xaml
+    /// </summary>
+    public partial class ChooseCordProductWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ChooseCordProductWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private List<CordProduct> allItems;
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        #endregion
+
+        #region Privete Methods
+
+        private void RefreshGrid()
+        {
+            // Reload all items
+            allItems = LobaclDb.Products;
+
+            grid.ItemsSource = null;
+
+            List<CordProduct> items;
+            items = allItems.FindAll((item) => !item.IsUsed);
+
+            grid.ItemsSource = items;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Setup.
+        /// </summary>
+        public void Setup()
+        {
+            if (null == LobaclDb.Products || LobaclDb.Products.Count <= 0)
+            {
+                LobaclDb.LoadCordProducts();
+            }
+
+            SelectedProduct = null;
+            RefreshGrid();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets Selected Product.
+        /// </summary>
+        public CordProduct SelectedProduct { get; private set; }
+
+        #endregion
+    }
+}
