5112d4f36db2430402e15a75a791ed40e75ff707
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-22 20:24:13 +0700

    M3 Cord Production System #382 - M3.Cord.Production.App re-implements #182.

---
+++ 05.Controls/M3.Cord.Controls/Controls/Documents/FirstTwist/S4x2/S4x2UpTwisterConditionControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M3.Cord.Controls.Documents.FirstTwist.S4x2
+{
+    /// <summary>
+    /// Interaction logic for S4x2UpTwisterConditionControl.xaml
+    /// </summary>
+    public partial class S4x2UpTwisterConditionControl : UserControl
+    {
+        public S4x2UpTwisterConditionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
