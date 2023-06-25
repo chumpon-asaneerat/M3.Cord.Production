62442088ff5585eb38972299e851442a7932df7a
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-25 18:19:02 +0700

    M3 Cord Production System #394 - M3.Cord.Production.App re-implements #194.

--- 00.NLib/NLib.Wpf.Controls/Controls/NPreForms/NPreFormColumn.cs
+++ 00.NLib/NLib.Wpf.Controls/Controls/NPreForms/NPreFormColumn.cs
@@ -56,6 +56,94 @@ namespace NLib.Wpf.Controls
 
         #region Public Properties
 
+        #region HeaderForeground
+
+        /// <summary>
+        /// The HeaderForegroundProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderForegroundProperty =
+            DependencyProperty.Register(
+                nameof(HeaderForeground),
+                typeof(Brush),
+                typeof(NPreFormColumn),
+                new FrameworkPropertyMetadata(Brushes.Black, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets HeaderForeground.
+        /// </summary>
+        public Brush HeaderForeground
+        {
+            get { return (Brush)GetValue(HeaderForegroundProperty); }
+            set { SetValue(HeaderForegroundProperty, value); }
+        }
+
+        #endregion
+
+        #region HeaderBackground
+
+        /// <summary>
+        /// The HeaderBackgroundProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.Register(
+                nameof(HeaderBackground),
+                typeof(Brush),
+                typeof(NPreFormColumn),
+                new FrameworkPropertyMetadata(Brushes.Silver, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets HeaderBackground.
+        /// </summary>
+        public Brush HeaderBackground
+        {
+            get { return (Brush)GetValue(HeaderBackgroundProperty); }
+            set { SetValue(HeaderBackgroundProperty, value); }
+        }
+
+        #endregion
+
+        #region HeaderText
+
+        /// <summary>
+        /// The HeaderTextProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderTextProperty =
+            DependencyProperty.Register(
+                nameof(HeaderText),
+                typeof(string),
+                typeof(NPreFormColumn),
+                new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets Header Text.
+        /// </summary>
+        public string HeaderText
+        {
+            get { return (string)GetValue(HeaderTextProperty); }
+            set { SetValue(HeaderTextProperty, value); }
+        }
+
+        #endregion
+
+        #region HeaderTextAlignment
+
+        /// <summary>
+        /// The HeaderTextAlignmentProperty Dependency property.
+        /// </summary>
+        public static readonly DependencyProperty HeaderTextAlignmentProperty =
+            DependencyProperty.Register(
+                nameof(HeaderTextAlignment),
+                typeof(TextAlignment),
+                typeof(NPreFormColumn),
+                new FrameworkPropertyMetadata(TextAlignment.Center, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        /// <summary>
+        /// Gets or sets HeaderTextAlignment.
+        /// </summary>
+        public TextAlignment HeaderTextAlignment
+        {
+            get { return (TextAlignment)GetValue(HeaderTextAlignmentProperty); }
+            set { SetValue(HeaderTextAlignmentProperty, value); }
+        }
+
+        #endregion
+
         #endregion
     }
 }
