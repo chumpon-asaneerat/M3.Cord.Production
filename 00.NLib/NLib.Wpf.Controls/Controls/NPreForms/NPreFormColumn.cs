04b6216c02b1a463005cd4d56cb02a00fd66c554
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-25 17:31:17 +0700

    M3 Cord Production System #390 - M3.Cord.Production.App re-implements #190.

---
+++ 00.NLib/NLib.Wpf.Controls/Controls/NPreForms/NPreFormColumn.cs
@@ -0,0 +1,61 @@
+﻿#region Using
+
+using NLib.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace NLib.Wpf.Controls
+{
+    /// <summary>
+    /// The NPreFormColumn Control.
+    /// </summary>
+    public class NPreFormColumn : Control
+    {
+        #region Constructor
+
+        static NPreFormColumn()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NPreFormColumn), new FrameworkPropertyMetadata(typeof(NPreFormColumn)));
+        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public NPreFormColumn()
+        {
+            // init
+            //Items = new ObservableCollection<NGroupMenuItem>();
+        }
+
+        #endregion
+
+        #region Override Methods
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+}
