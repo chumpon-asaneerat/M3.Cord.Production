6f8879dea30edc268cf188c8d63c7e63194141cc
chumpon-asaneerat <chumponsenate@gmail.com>
2023-06-26 09:15:10 +0700

    M3 Cord Production System #398 - M3.Cord.Production.App re-implements #198.

---
+++ 05.Controls/M3.Cord.Controls/Pages/FirstTwist/S4x1UpTwisterConditionPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for S4x1UpTwisterConditionPage.xaml
+    /// </summary>
+    public partial class S4x1UpTwisterConditionPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public S4x1UpTwisterConditionPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+}
