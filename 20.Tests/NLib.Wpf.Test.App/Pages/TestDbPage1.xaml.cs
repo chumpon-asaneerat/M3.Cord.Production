a432e9e0e5faaf2e6515f87e32c08c31bd03d86e
chumpon-asaneerat <chumponsenate@gmail.com>
2023-07-16 09:14:46 +0700

    M3 Cord Production System #434 - M3.Cord.Production.App re-implements #234.

---
+++ 20.Tests/NLib.Wpf.Test.App/Pages/TestDbPage1.xaml.cs
@@ -0,0 +1,52 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+using M3.Cord;
+
+namespace NLib.Wpf.Test.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for TestDbPage1.xaml
+    /// </summary>
+    public partial class TestDbPage1 : UserControl
+    {
+        #region Constructor
+
+        public TestDbPage1()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            DbServer.Instance.Start();
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DbServer.Instance.Shutdown();
+        }
+
+        #endregion
+    }
+}
