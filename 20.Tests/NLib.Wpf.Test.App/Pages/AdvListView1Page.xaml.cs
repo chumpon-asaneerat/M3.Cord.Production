2fa7a94fa086d899f0f5532079591805d5dbfd26
chumpon-asaneerat <chumponsenate@gmail.com>
2023-07-20 14:16:19 +0700

    M3 Cord Production System #446 - M3.Cord.Production.App re-implements #245.   - Implements Advance ListView for Condition Documents.

---
+++ 20.Tests/NLib.Wpf.Test.App/Pages/AdvListView1Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NLib.Wpf.Test.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for AdvListView1Page.xaml
+    /// </summary>
+    public partial class AdvListView1Page : UserControl
+    {
+        public AdvListView1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
