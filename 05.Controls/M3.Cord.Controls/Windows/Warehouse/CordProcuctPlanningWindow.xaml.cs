07d6d7b76769a1eed8ff04a4211f101a7803ce1d
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-06 23:11:18 +0700

    M3 Cord Production System #460 - M3.Cord.Production.App re-implements #260.

--- 05.Controls/M3.Cord.Controls/Windows/Warehouse/CordProcuctPlanningWindow.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Windows/Warehouse/CordProcuctPlanningWindow.xaml.cs
@@ -54,12 +54,31 @@ namespace M3.Cord.Windows
 
         private void cmdOk_Click(object sender, RoutedEventArgs e)
         {
+            // Item Yarn
             if (null != cbItemYanrs.SelectedItem && cbItemYanrs.SelectedItem is CordItemYarn)
             {
                 var itemYarn = cbItemYanrs.SelectedItem as CordItemYarn;
                 if (null != _item)
                 {
                     _item.ItemYarn = itemYarn.ItemYarn;
+                }
+            }
+            // Customer
+            if (null != cbCustomers.SelectedItem && cbCustomers.SelectedItem is Customer)
+            {
+                var customer = cbCustomers.SelectedItem as Customer;
+                if (null != _item)
+                {
+                    _item.CustomerName = customer.CustomerName;
+                }
+            }
+            // Item Code
+            if (null != cbItemCodes.SelectedItem && cbItemCodes.SelectedItem is ProductItemCode)
+            {
+                var itemCode = cbItemCodes.SelectedItem as ProductItemCode;
+                if (null != _item)
+                {
+                    _item.ItemCode = itemCode.ItemCode;
                 }
             }
 
@@ -72,10 +91,21 @@ namespace M3.Cord.Windows
 
         private void LoadComboBoxes()
         {
+            // Item Yarn
             cbItemYanrs.ItemsSource = null;
 
             var itemYarns = CordItemYarn.Gets().Value();
             cbItemYanrs.ItemsSource = itemYarns;
+
+            // Customer
+            cbCustomers.ItemsSource = null;
+            var customers = Customer.Gets().Value();
+            cbCustomers.ItemsSource = customers;
+
+            // Item Code
+            cbItemCodes.ItemsSource = null;
+            var itemCodes = ProductItemCode.Gets().Value();
+            cbItemCodes.ItemsSource = itemCodes;
         }
 
         #endregion
@@ -87,6 +117,8 @@ namespace M3.Cord.Windows
             LoadComboBoxes();
 
             var itemYarns = cbItemYanrs.ItemsSource as List<CordItemYarn>;
+            var customers = cbCustomers.ItemsSource as List<Customer>;
+            var itemCodes = cbItemCodes.ItemsSource as List<ProductItemCode>;
 
             _item = item;
             this.DataContext = _item;
@@ -95,14 +127,37 @@ namespace M3.Cord.Windows
             {
                 _item.CalcTotals();
 
+                // ItemYarn
                 int idx = -1;
                 if (null != itemYarns)
                 {
                     idx = itemYarns.FindIndex(yarn => { return yarn.ItemYarn == item.ItemYarn; });
                 }
-                this.InvokeAction(() => 
+                this.InvokeAction(() =>
                 {
                     cbItemYanrs.SelectedIndex = idx;
+                });
+
+                // Customer
+                int idx2 = -1;
+                if (null != customers)
+                {
+                    idx2 = customers.FindIndex(customer => { return customer.CustomerName == item.CustomerName; });
+                }
+                this.InvokeAction(() =>
+                {
+                    cbCustomers.SelectedIndex = idx2;
+                });
+
+                // Item Code
+                int idx3 = -1;
+                if (null != itemCodes)
+                {
+                    idx3 = itemCodes.FindIndex(itemCode => { return itemCode.ItemCode == item.ItemCode; });
+                }
+                this.InvokeAction(() =>
+                {
+                    cbItemCodes.SelectedIndex = idx3;
                 });
             }
         }
