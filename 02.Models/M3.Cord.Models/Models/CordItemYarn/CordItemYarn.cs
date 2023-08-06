ff72bf2293239669026e60a27319488c561ae4e2
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-06 21:16:41 +0700

    M3 Cord Production System #453 - M3.Cord.Production.App re-implements #253.

---
+++ 02.Models/M3.Cord.Models/Models/CordItemYarn/CordItemYarn.cs
@@ -0,0 +1,82 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.Cord.Models
+{
+    public class CordItemYarn
+    {
+        #region Public Proeprties
+
+        public string ItemYarn { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Gets
+        /// </summary>
+        /// <returns></returns>
+        public static NDbResult<List<CordItemYarn>> Gets()
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<CordItemYarn>> rets = new NDbResult<List<CordItemYarn>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            var p = new DynamicParameters();
+
+            try
+            {
+                var items = cnn.Query<CordItemYarn>("GetItemYarns", p,
+                    commandType: CommandType.StoredProcedure);
+                var data = (null != items) ? items.ToList() : null;
+                rets.Success(data);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            if (null == rets.data)
+            {
+                // create empty list.
+                rets.data = new List<CordItemYarn>();
+            }
+
+            return rets;
+        }
+
+        #endregion
+    }
+}
