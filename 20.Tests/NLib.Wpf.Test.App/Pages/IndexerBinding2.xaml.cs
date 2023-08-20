c03c1f24d33b71ee9586ca5f51568301ad87b5d8
chumpon-asaneerat <chumponsenate@gmail.com>
2023-08-21 01:17:26 +0700

    M3 Cord Production System #484 - M3.Cord.Production.App re-implements #283

---
+++ 20.Tests/NLib.Wpf.Test.App/Pages/IndexerBinding2.xaml.cs
@@ -0,0 +1,57 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace NLib.Wpf.Test.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for IndexerBinding2.xaml
+    /// </summary>
+    public partial class IndexerBinding2 : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public IndexerBinding2()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            Init();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Init()
+        {
+
+        }
+
+        #endregion
+    }
+}
