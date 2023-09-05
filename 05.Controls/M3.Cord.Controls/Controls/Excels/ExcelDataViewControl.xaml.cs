b888a29ef686d292063aaad276136e555db425a7
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-06 02:55:57 +0700

    M3 Cord Production System #523 - M3.Cord.Production.App re-implements #316

--- 05.Controls/M3.Cord.Controls/Controls/Excels/ExcelDataViewControl.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Controls/Excels/ExcelDataViewControl.xaml.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+using NLib;
+using NLib.Reflection;
+using NLib.Services;
+using M3.Cord.Models;
+
+#endregion
 
 namespace M3.Cord.Controls.Excels
 {
@@ -20,9 +22,83 @@ namespace M3.Cord.Controls.Excels
     /// </summary>
     public partial class ExcelDataViewControl : UserControl
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public ExcelDataViewControl()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Internal Variables
+
+        private NExcelWorksheet _sheet = null;
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup<T>(NExcelWorksheet sheet)
+            where T : class, new()
+        {
+            _sheet = sheet;
+            LoadItems<T>();
+        }
+
+        private void LoadItems<T>()
+            where T : class, new()
+        {
+            Items = _sheet.LoadItems<T>();
+            var maps = _sheet.Mappings;
+
+            if (null == maps || maps.Count <= 0)
+            {
+                // Clear exist data
+                this.lvMapPreview.ItemsSource = null;
+                // Clear exist columns
+                this.lvMapGridView.Columns.Clear();
+
+                return;
+            }
+
+            // Do work while the dispatcher processing is disabled.
+            using (Dispatcher.DisableProcessing())
+            {
+                // Clear exist data
+                this.lvMapPreview.ItemsSource = null;
+                // Clear exist columns
+                this.lvMapGridView.Columns.Clear();
+
+                // Build new columns
+                GridViewColumn col;
+                foreach (var map in maps)
+                {
+                    col = new GridViewColumn();
+                    col.Header = map.DisplayText;
+                    col.Width = 120;
+                    col.DisplayMemberBinding = new Binding(map.PropertyName);
+
+                    this.lvMapGridView.Columns.Add(col);
+                }
+
+                // set new ItemsSource
+                this.lvMapPreview.ItemsSource = Items;
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets Items.
+        /// </summary>
+        public System.Collections.IList Items { get; private set; }
+
+        #endregion
     }
 }
