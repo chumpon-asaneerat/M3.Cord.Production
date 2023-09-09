a805b1b0a1b8e0686cbac9efb1cbf35393cd3338
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-09 23:08:27 +0700

    M3 Cord Production System #528 - M3.Cord.Production.App re-implements #321

---
+++ 05.Controls/M3.Cord.Controls/_Templates/CordSamplingDetialsDocument.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace M3.Cord._Templates
+{
+    /// <summary>
+    /// Interaction logic for CordSamplingDetialsDocument.xaml
+    /// </summary>
+    public partial class CordSamplingDetialsDocument : UserControl
+    {
+        public CordSamplingDetialsDocument()
+        {
+            InitializeComponent();
+        }
+    }
+}
