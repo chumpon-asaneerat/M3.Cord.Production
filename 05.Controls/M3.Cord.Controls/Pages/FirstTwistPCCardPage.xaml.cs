8c910f2738fa4c6d14ee966365cadbdab5bf5c47
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-14 12:37:53 +0700

    M3 Cord Production System #569 - M3.Cord.Production.App re-implements #359

---
+++ 05.Controls/M3.Cord.Controls/Pages/FirstTwistPCCardPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for FirstTwistPCCardPage.xaml
+    /// </summary>
+    public partial class FirstTwistPCCardPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public FirstTwistPCCardPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
