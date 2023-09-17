5f8a94ee9eab0c032fda64355f3924b404708550
chumpon-asaneerat <chumponsenate@gmail.com>
2023-09-18 01:02:35 +0700

    M3 Cord Production System #588 - M3.Cord.Production.App re-implements #378

---
+++ 05.Controls/M3.Cord.Controls/Pages/FirstTwistS4x2ConditionPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for FirstTwistS4x2ConditionPage.xaml
+    /// </summary>
+    public partial class FirstTwistS4x2ConditionPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public FirstTwistS4x2ConditionPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private FirstTwistMC selectedMC;
+        private PCTwist1 pcCard;
+        private PCTwist1Operation _operation = null;
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            // First Twist
+            var page = M3CordApp.Pages.FirstTwistMC;
+            page.Setup();
+            PageContentManager.Instance.Current = page;
+        }
+
+        private void cmdSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup(FirstTwistMC mc)
+        {
+            selectedMC = mc;
+        }
+
+        #endregion
+    }
+}
