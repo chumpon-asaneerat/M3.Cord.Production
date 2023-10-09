61ddb64c628d0e4da70f0035048d21012832b5da
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-09 10:26:43 +0700

    M3 Cord Production System #618 - M3.Cord.Production.App re-implements #408

---
+++ 05.Controls/M3.Cord.Controls/Pages/PalletSettingManagePage.xaml.cs
@@ -0,0 +1,86 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for PalletSettingManagePage.xaml
+    /// </summary>
+    public partial class PalletSettingManagePage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PalletSettingManagePage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            M3CordApp.Pages.GotoCordMainMenu();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void RefreshGrid()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+            RefreshGrid();
+        }
+
+        #endregion
+    }
+}
