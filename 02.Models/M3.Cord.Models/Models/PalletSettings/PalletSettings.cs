8f2dab4061e7aa81bea699c704dfdd01c1203b7b
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-15 22:45:18 +0700

    M3 Cord Production System #657 - M3.Cord.Production.App re-implements #447

--- 02.Models/M3.Cord.Models/Models/PalletSettings/PalletSettings.cs
+++ 02.Models/M3.Cord.Models/Models/PalletSettings/PalletSettings.cs
@@ -30,6 +30,7 @@ namespace M3.Cord.Models
 
     public enum PalletStatus : int
     {
+        All = -1,
         Create = 0,
         Ageing = 1,
         Dip = 2
@@ -88,7 +89,44 @@ namespace M3.Cord.Models
         // Runtime data
         public string UserName { get; set; }
         public PalletType PalletType { get; set; }
+
+        public string PalletTypeText
+        {
+            get
+            {
+                if (PalletType == PalletType.Pallet)
+                {
+                    return "Pallet";
+                }
+                else
+                {
+                    return "Cart";
+                }
+            }
+            set { }
+        }
+
         public PalletStatus PalletStatus { get; set; }
+
+        public string PalletStatusText 
+        {
+            get 
+            { 
+                if (PalletStatus == PalletStatus.Ageing)
+                {
+                    return "Ageing";
+                }
+                else if (PalletStatus == PalletStatus.Dip)
+                {
+                    return "DIP";
+                }
+                else
+                {
+                    return "Create";
+                }
+            }
+            set { }
+        }
 
         public int? PCId { get; set; }
         public DateTime? PCDate { get; set; }
@@ -200,7 +238,7 @@ namespace M3.Cord.Models
         /// Gets
         /// </summary>
         /// <returns></returns>
-        public static NDbResult<List<PalletSetting>> Gets()
+        public static NDbResult<List<PalletSetting>> Gets(PalletStatus palletStatus)
         {
             MethodBase med = MethodBase.GetCurrentMethod();
 
@@ -218,8 +256,15 @@ namespace M3.Cord.Models
                 return rets;
             }
 
+            int? status = new int?();
+            if (palletStatus != PalletStatus.All)
+            {
+                status = new int?((int)palletStatus);
+            }
+
             var p = new DynamicParameters();
-            
+            p.Add("@PalletStatus", status);
+
             try
             {
                 var items = cnn.Query<PalletSetting>("GetPalletSettings", p,
@@ -258,7 +303,8 @@ namespace M3.Cord.Models
         /// Search
         /// </summary>
         /// <returns></returns>
-        public static NDbResult<PalletSetting> Search(string palletCode)
+        public static NDbResult<PalletSetting> Search(string palletCode,
+            PalletStatus palletStatus)
         {
             MethodBase med = MethodBase.GetCurrentMethod();
 
@@ -276,8 +322,15 @@ namespace M3.Cord.Models
                 return ret;
             }
 
+            int? status = new int?();
+            if (palletStatus != PalletStatus.All)
+            {
+                status = new int?((int)palletStatus);
+            }
+
             var p = new DynamicParameters();
             p.Add("@PalletCode", palletCode);
+            p.Add("@PalletStatus", status);
 
             try
             {
@@ -301,8 +354,9 @@ namespace M3.Cord.Models
         /// Search
         /// </summary>
         /// <returns></returns>
-        public static NDbResult<List<PalletSetting>> Search(string productLotNo, 
-            DateTime? begin, DateTime? end, string productCode)
+        public static NDbResult<List<PalletSetting>> Search(string productLotNo,
+            DateTime? begin, DateTime? end, string productCode,
+            PalletStatus palletStatus)
         {
             MethodBase med = MethodBase.GetCurrentMethod();
 
@@ -320,11 +374,18 @@ namespace M3.Cord.Models
                 return rets;
             }
 
+            int? status = new int?();
+            if (palletStatus != PalletStatus.All)
+            {
+                status = new int?((int)palletStatus);
+            }
+
             var p = new DynamicParameters();
             p.Add("@ProductLotNo", productLotNo);
             p.Add("@beginDate", begin);
             p.Add("@endDate", end);
             p.Add("@ProductCode", productCode);
+            p.Add("@PalletStatus", status);
 
             try
             {
@@ -424,6 +485,40 @@ namespace M3.Cord.Models
             }
 
             return ret;
+        }
+
+        /// <summary>
+        /// Update Pallet Status
+        /// </summary>
+        public static void UpdatePalletStatus(int palletId, PalletStatus palletStatus)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<PalletSetting> ret = new NDbResult<PalletSetting>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                return;
+            }
+
+            if (palletStatus == PalletStatus.All)
+                return;
+
+            var p = new DynamicParameters();
+            p.Add("@PalletId", palletId);
+            p.Add("@PalletStatus", (int)palletStatus);
+
+            try
+            {
+                cnn.Execute("UpdatePalletSettingStatus", p, commandType: CommandType.StoredProcedure);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+            }
         }
 
         #endregion
