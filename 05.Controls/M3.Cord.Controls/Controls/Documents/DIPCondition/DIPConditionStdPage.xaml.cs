5a66e7cd5ba10864bae3e8b8a3487b524050aa83
vorakanok <vorakanok@yahoo.com>
2023-10-23 16:55:08 +0700

    Update DIPCondition

---
+++ 05.Controls/M3.Cord.Controls/Controls/Documents/DIPCondition/DIPConditionStdPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+using NLib.Reflection;
+
+#endregion
+
+namespace M3.Cord.Controls.Documents
+{
+    /// <summary>
+    /// Interaction logic for DIPConditionStdPage.xaml
+    /// </summary>
+    public partial class DIPConditionStdPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DIPConditionStdPage()
+        {
+            InitializeComponent();
+
+            this.DataContext = null;
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+
+        #endregion
+    }
+}
