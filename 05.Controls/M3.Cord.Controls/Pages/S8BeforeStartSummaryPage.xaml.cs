ba32f93e8e3d71871ef14e92747fa530714f092f
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-25 13:39:55 +0700

    M3 Cord Production System #678 - M3.Cord.Production.App re-implements #468

---
+++ 05.Controls/M3.Cord.Controls/Pages/S8BeforeStartSummaryPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+
+#endregion
+
+namespace M3.Cord.Pages
+{
+    /// <summary>
+    /// Interaction logic for S8BeforeStartSummaryPage.xaml
+    /// </summary>
+    public partial class S8BeforeStartSummaryPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public S8BeforeStartSummaryPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            M3CordApp.Pages.GotoCordMainMenu();
+        }
+
+        private void cmdSave_Click(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Save()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+        }
+
+        public void RefreshGrid()
+        {
+
+        }
+
+        #endregion
+    }
+}
