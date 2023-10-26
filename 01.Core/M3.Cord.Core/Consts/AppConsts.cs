e701d66c29e767c48a7f6a3c741ff82167a68594
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-26 13:17:56 +0700

    M3 Cord Production System #701 - M3.Cord.Production.App re-implements #491   Update App build 805

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "751";
-                    public static DateTime LastUpdate = new DateTime(2023, 10, 16, 12, 00, 00);
+                    public static string Build = "805";
+                    public static DateTime LastUpdate = new DateTime(2023, 10, 26, 13, 30, 00);
                 }
 
                 public static class QA
@@ -33,8 +33,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "751";
-                    public static DateTime LastUpdate = new DateTime(2023, 10, 16, 12, 00, 00);
+                    public static string Build = "805";
+                    public static DateTime LastUpdate = new DateTime(2023, 10, 26, 13, 30, 00);
                 }
 
                 public static class Developer
@@ -43,8 +43,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "751";
-                    public static DateTime LastUpdate = new DateTime(2023, 10, 16, 12, 00, 00);
+                    public static string Build = "805";
+                    public static DateTime LastUpdate = new DateTime(2023, 10, 26, 13, 30, 00);
                 }
             }
         }
