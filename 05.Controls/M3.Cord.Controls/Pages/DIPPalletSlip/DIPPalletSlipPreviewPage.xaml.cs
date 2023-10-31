a2b363442e076aa667a564a37e451dc247cf6b10
chumpon-asaneerat <chumponsenate@gmail.com>
2023-10-31 18:03:51 +0700

    M3 Cord Production System #706 - M3.Cord.Production.App re-implements #496

--- 05.Controls/M3.Cord.Controls/Pages/DIPPalletSlip/DIPPalletSlipPreviewPage.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Pages/DIPPalletSlip/DIPPalletSlipPreviewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#region Using
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using NLib.Services;
+using M3.Cord.Models;
+using NLib.Models;
+using NLib;
+using NLib.Reports.Rdlc;
+using System.Reflection;
+
+#endregion
+
 namespace M3.Cord.Pages
 {
     /// <summary>
@@ -20,9 +31,190 @@ namespace M3.Cord.Pages
     /// </summary>
     public partial class DIPPalletSlipPreviewPage : UserControl
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public DIPPalletSlipPreviewPage()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Internal Variables
+
+        private bool _reprint = false;
+        private List<DIPPalletSlip> _items = null;
+
+        #endregion
+
+        #region Loaded/Unloaded
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdHome_Click(object sender, RoutedEventArgs e)
+        {
+            GoBack();
+        }
+
+        private void cmdPrint_Click(object sender, RoutedEventArgs e)
+        {
+            Print();
+        }
+
+        #endregion
+
+        private void Print()
+        {
+            cmdPrint.Visibility = Visibility.Collapsed;
+
+            MethodBase med = MethodBase.GetCurrentMethod();
+            try
+            {
+                if (null != _items)
+                {
+                    foreach (var item in _items)
+                    {
+                        if (!_reprint)
+                        {
+                            /*
+                            // Create new
+                            DIPPalletSlip.Save(item);
+                            if (item.PalletId.HasValue)
+                            {
+                                // update id
+                                var pCode = DIPPalletCode.GetLastId(item.MCCode).Value();
+                                if (pCode != null)
+                                {
+                                    DIPPalletCode.UpdateLastId(item.MCCode, pCode.LastId + 1);
+                                }
+
+                                foreach (var item2 in item.Items)
+                                {
+                                    item2.PalletId = item.PalletId.Value;
+                                    DIPPalletSlip.Save(item2);
+                                }
+                            }
+                            */
+                        }
+                    }
+                    this.rptViewer.Print(ReportDisplayName);
+                }
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+            }
+
+            cmdPrint.Visibility = Visibility.Visible;
+
+            GoBack();
+        }
+
+        private void GoBack()
+        {
+            /*
+            // Back to DIP Pallet Slip Manage page
+            var page = M3CordApp.Pages.DIPPalletSlipManage;
+            page.Setup();
+            PageContentManager.Instance.Current = page;
+            */
+            M3CordApp.Pages.GotoCordMainMenu();
+        }
+
+        #region Report methods
+
+        private string ReportDisplayName
+        {
+            get { return "DIPPalletSlip." + DateTime.Now.ToThaiDateTimeString("ddMMyyyyHHmmssfff"); }
+        }
+
+        private RdlcReportModel GetReportModel()
+        {
+            Assembly assembly = this.GetType().Assembly;
+            RdlcReportModel inst = new RdlcReportModel();
+
+            // Set Display Name (default file name).
+            inst.DisplayName = ReportDisplayName;
+
+            inst.Definition.EmbededReportName = "M3.Cord.Reports.PalletSettingA6.rdlc";
+            inst.Definition.RdlcInstance = RdlcReportUtils.GetEmbededReport(assembly,
+                inst.Definition.EmbededReportName);
+            // clear reprot datasource.
+            inst.DataSources.Clear();
+
+            List<DIPPalletSlip> items = new List<DIPPalletSlip>();
+            if (null != _items)
+            {
+                foreach (var item in _items)
+                {
+                    items.Add(item); // Add new because is blank.
+                }
+            }
+
+            // assign new data source
+            RdlcReportDataSource mainDS = new RdlcReportDataSource();
+            mainDS.Name = "main"; // the datasource name in the rdlc report.
+            mainDS.Items = items; // setup data source
+            // Add to datasources
+            inst.DataSources.Add(mainDS);
+
+            // Add parameters (if required).
+            DateTime today = DateTime.Now;
+            string printDate = today.ToThaiDateTimeString("dd/MM/yyyy HH:mm:ss");
+            inst.Parameters.Add(RdlcReportParameter.Create("PrintDate", printDate));
+
+            return inst;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup(List<DIPPalletSlip> items, bool reprint)
+        {
+            _reprint = reprint;
+            _items = items;
+
+            var model = GetReportModel();
+            if (null == model ||
+                null == model.DataSources || model.DataSources.Count <= 0 ||
+                null == model.DataSources[0] || null == model.DataSources[0].Items)
+            {
+                this.rptViewer.ClearReport();
+            }
+            else
+            {
+                MethodBase med = MethodBase.GetCurrentMethod();
+                try
+                {
+                    this.rptViewer.LoadReport(model);
+                }
+                catch (Exception ex)
+                {
+                    med.Err(ex);
+                }
+                finally
+                {
+                    this.rptViewer.RefreshReport();
+                }
+            }
+        }
+
+        #endregion
     }
 }
