b7e7a1ed3f496a81f44c0f7cd5cd714a10d19684
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-03 12:37:23 +0700

    M3 Cord Production System #806 - M3.Cord.Production.App re-implements #596   - Solution update   - compile build 985

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "970";
-                    public static DateTime LastUpdate = new DateTime(2023, 12, 27, 21, 45, 00);
+                    public static string Build = "985";
+                    public static DateTime LastUpdate = new DateTime(2024, 01, 03, 12, 25, 00);
                 }
 
                 public static class QA
