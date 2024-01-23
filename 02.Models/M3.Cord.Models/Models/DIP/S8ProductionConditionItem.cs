c8901afe64b701af26d96534d9834bae84766afe
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-24 02:06:10 +0700

    M3 Cord Production System #858 - M3.Cord.Production.App re-implements #74   - S8ProductionCondition #1

---
+++ 02.Models/M3.Cord.Models/Models/DIP/S8ProductionConditionItem.cs
@@ -0,0 +1,25 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.Cord.Models
+{
+    public class S8ProductionConditionItem
+    {
+    }
+}
---
+++ 02.Models/M3.Cord.Models/Models/DIP/S8ProductionConditionItemStd.cs
@@ -0,0 +1,189 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.Cord.Models
+{
+    public class S8ProductionConditionItemStd
+    {
+        #region Public Proeprties
+
+        public string ProductCode { get; set; }
+        public bool? StretchDSC { get; set; }
+        public decimal? StretchD { get; set; }
+        public decimal? StretchDErr { get; set; }
+        public bool? StretchHSC { get; set; }
+        public decimal? StretchH { get; set; }
+        public decimal? StretchHErr { get; set; }
+        public bool? StretchNSC { get; set; }
+        public decimal? StretchN { get; set; }
+        public decimal? StretchNErr { get; set; }
+        public bool? TempDSC { get; set; }
+        public decimal? TempD { get; set; }
+        public decimal? TempDErr { get; set; }
+        public bool? TempHNSC { get; set; }
+        public decimal? TempHN { get; set; }
+        public decimal? TempHNErr { get; set; }
+        public bool? SpeedSC { get; set; }
+        public decimal? Speed { get; set; }
+        public decimal? SpeedErr { get; set; }
+        public bool? TreatSC { get; set; }
+        public decimal? Treat { get; set; }
+        public bool? DoffingLengthSC { get; set; }
+        public decimal? DoffingLength { get; set; }
+        public bool? WeightSC { get; set; }
+        public decimal? Weight { get; set; }
+        public bool? SpindleSC { get; set; }
+        public decimal? Spindle { get; set; }
+        public bool? ProductionGoodSC { get; set; }
+        public decimal? ProductionGood { get; set; }
+        public bool? ProductionTotalSC { get; set; }
+        public decimal? ProductionTotal { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static NDbResult<List<S8ProductionConditionItemStd>> Gets(string productCode)
+        {
+            NDbResult<List<S8ProductionConditionItemStd>> rets = new NDbResult<List<S8ProductionConditionItemStd>>();
+
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ProductCode", productCode);
+
+            try
+            {
+                var items = cnn.Query<S8ProductionConditionItemStd>("GetS8ProductionConditionItemStd", p,
+                    commandType: CommandType.StoredProcedure);
+                var data = (null != items) ? items.ToList() : null;
+                rets.Success(data);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            return rets;
+        }
+
+        /// <summary>
+        /// Save
+        /// </summary>
+        /// <param name="value">The ProductionRecordStd item to save.</param>
+        /// <returns></returns>
+        public static NDbResult<S8ProductionConditionItemStd> Save(S8ProductionConditionItemStd value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<S8ProductionConditionItemStd> ret = new NDbResult<S8ProductionConditionItemStd>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@ProductCode", value.ProductCode);
+            p.Add("@StretchDSC", value.StretchDSC);
+            p.Add("@StretchD", value.StretchD);
+            p.Add("@StretchDErr", value.StretchDErr);
+            p.Add("@StretchHSC", value.StretchHSC);
+            p.Add("@StretchH", value.StretchH);
+            p.Add("@StretchHErr", value.StretchHErr);
+            p.Add("@StretchNSC", value.StretchNSC);
+            p.Add("@StretchN", value.StretchN);
+            p.Add("@StretchNErr", value.StretchNErr);
+            p.Add("@TempDSC", value.TempDSC);
+            p.Add("@TempD", value.TempD);
+            p.Add("@TempDErr", value.TempDErr);
+            p.Add("@TempHNSC", value.TempHNSC);
+            p.Add("@TempHN", value.TempHN);
+            p.Add("@TempHNErr", value.TempHNErr);
+            p.Add("@SpeedSC", value.SpeedSC);
+            p.Add("@Speed", value.Speed);
+            p.Add("@SpeedErr", value.SpeedErr);
+            p.Add("@TreatSC", value.TreatSC);
+            p.Add("@Treat", value.Treat);
+            p.Add("@DoffingLengthSC", value.DoffingLengthSC);
+            p.Add("@DoffingLength", value.DoffingLength);
+            p.Add("@WeightSC", value.WeightSC);
+            p.Add("@Weight", value.Weight);
+            p.Add("@SpindleSC", value.SpindleSC);
+            p.Add("@Spindle", value.Spindle);
+            p.Add("@ProductionGoodSC", value.ProductionGoodSC);
+            p.Add("@ProductionGood", value.ProductionGood);
+            p.Add("@ProductionTotalSC", value.ProductionTotalSC);
+            p.Add("@ProductionTotal", value.ProductionTotal);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveS8ProductionConditionItemStd", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        #endregion
+    }
+}
