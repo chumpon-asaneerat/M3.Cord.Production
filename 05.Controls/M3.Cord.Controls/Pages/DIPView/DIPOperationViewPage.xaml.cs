2fe760cfeb077270b3c1717776202d5877ceac4f
chumpon-asaneerat <chumponsenate@gmail.com>
2024-01-31 06:18:01 +0700

    M3 Cord Production System #922 - M3.Cord.Production.App re-implements #138   - Implements DIPView pages #15

--- 05.Controls/M3.Cord.Controls/Pages/DIPView/DIPOperationViewPage.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Pages/DIPView/DIPOperationViewPage.xaml.cs
@@ -95,11 +95,9 @@ namespace M3.Cord.Pages
 
         private void cmdDIPCondition_Click(object sender, RoutedEventArgs e)
         {
-            /*
             var page = M3CordApp.Pages.DIPConditionView;
             page.Setup(mc, pcCard);
             PageContentManager.Instance.Current = page;
-            */
         }
 
         private void cmdS8ConditionSummary_Click(object sender, RoutedEventArgs e)
