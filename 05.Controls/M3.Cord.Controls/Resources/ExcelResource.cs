4385cb1212c119e5d039f9114ad58fca85b814ae
chumpon-asaneerat <chumponsenate@gmail.com>
2024-02-04 22:33:04 +0700

    M3 Cord Production System #933 - M3.Cord.Production.App re-implements #149   - Implements Excel exports #1

---
+++ 05.Controls/M3.Cord.Controls/Resources/ExcelResource.cs
@@ -0,0 +1,136 @@
+﻿using NLib.Utils;
+using NLib;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace M3.Cord
+{
+    internal class ExcelResource
+    {
+        public const string S1 = "M3.Cord.Resources.Excels.FM4006_Sx1.xls";
+        public const string S4x2 = "M3.Cord.Resources.Excels.FM4006_Sx4x1.xls";
+        public const string S4x1 = "M3.Cord.Resources.Excels.FM4006_Sx4x2.xls";
+    }
+
+    public sealed class ExcelExportUtils
+    {
+        #region Create Resource
+
+        private static bool CreateResource(string resourceName,
+            string FullFileName, bool AutoOverwrite)
+        {
+            if (File.Exists(FullFileName))
+            {
+                if (!AutoOverwrite)
+                    throw (new Exception("File " + FullFileName + " is Exist"));
+                File.Delete(FullFileName);
+            }
+
+            using (WPFResourceAccess resAccess = new WPFResourceAccess())
+            {
+                ResourceStreamOptions option = new ResourceStreamOptions()
+                {
+                    ResourceName = resourceName,
+                    CallerType = typeof(MsDocumentUtils),
+                    TargetPath = Path.GetDirectoryName(FullFileName),
+                    TargetFileName = Path.GetFileName(FullFileName) + ".xls"
+                };
+
+                resAccess.CreateFile(option);
+            }
+
+            return File.Exists(FullFileName + ".xls");
+        }
+
+        private static void MoveFile(string sourceFileName, string targetFileName)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+            // move file
+            if (File.Exists(sourceFileName))
+            {
+                try
+                {
+                    File.Move(sourceFileName, targetFileName);
+                }
+                catch (Exception ex)
+                {
+                    ex.Err(med);
+                }
+            }
+        }
+
+        private static void DeleteFile(string fileName)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+            // delete file
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    File.Delete(fileName);
+                }
+                catch (Exception ex)
+                {
+                    ex.Err(med);
+                }
+            }
+        }
+
+        private static bool CreateFileFromResource(string embededResourceName,
+            string outputFileNameOnly, string FullFileName, bool AutoOverwrite)
+        {
+            if (string.IsNullOrWhiteSpace(embededResourceName) ||
+                string.IsNullOrWhiteSpace(outputFileNameOnly) ||
+                string.IsNullOrWhiteSpace(FullFileName))
+                return false;
+
+            if (CreateResource(embededResourceName, FullFileName, AutoOverwrite))
+            {
+                // Rename
+                string resourceFileOutput =
+                    Path.GetDirectoryName(FullFileName) + outputFileNameOnly;
+                // Move resource output file  to target file
+                MoveFile(resourceFileOutput, FullFileName);
+            }
+
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Create S1 File
+        /// </summary>
+        /// <param name="FullFileName">FileName</param>
+        /// <param name="AutoOverwrite">Force Overwrite</param>
+        /// <returns>true if file is created</returns>
+        public static bool CreateS1File(string FullFileName, bool AutoOverwrite)
+        {
+            return CreateFileFromResource(ExcelResource.S1,
+                @"\FM4006_Sx1.xls", FullFileName, AutoOverwrite);
+        }
+        /// <summary>
+        /// Create S4x1 File
+        /// </summary>
+        /// <param name="FullFileName">FileName</param>
+        /// <param name="AutoOverwrite">Force Overwrite</param>
+        /// <returns>true if file is created</returns>
+        public static bool CreateS4x1File(string FullFileName, bool AutoOverwrite)
+        {
+            return CreateFileFromResource(ExcelResource.S4x1,
+                @"\FM4006_Sx1.xls", FullFileName, AutoOverwrite);
+        }
+        /// <summary>
+        /// Create S4x2 File
+        /// </summary>
+        /// <param name="FullFileName">FileName</param>
+        /// <param name="AutoOverwrite">Force Overwrite</param>
+        /// <returns>true if file is created</returns>
+        public static bool CreateS4x2File(string FullFileName, bool AutoOverwrite)
+        {
+            return CreateFileFromResource(ExcelResource.S4x2,
+                @"\FM4006_Sx2.xls", FullFileName, AutoOverwrite);
+        }
+    }
+}
