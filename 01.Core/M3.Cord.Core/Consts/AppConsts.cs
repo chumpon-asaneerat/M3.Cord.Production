5870d56d2ee7a7dc4f0417cf8b6dc8d87859d472
chumpon-asaneerat <chumponsenate@gmail.com>
2024-02-12 12:51:06 +0700

    M3 Cord Production System #957 - Update App build 1290

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "1281";
-                    public static DateTime LastUpdate = new DateTime(2024, 02, 07, 12, 40, 00);
+                    public static string Build = "1290";
+                    public static DateTime LastUpdate = new DateTime(2024, 02, 12, 13, 00, 00);
                 }
 
                 public static class QA
@@ -33,8 +33,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "915";
-                    public static DateTime LastUpdate = new DateTime(2023, 12, 06, 06, 00, 00);
+                    public static string Build = "920";
+                    public static DateTime LastUpdate = new DateTime(2023, 12, 12, 13, 00, 00);
                 }
 
                 public static class Developer
