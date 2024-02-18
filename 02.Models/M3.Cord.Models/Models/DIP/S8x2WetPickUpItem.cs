e873b42b8b1f530fec65c67269baf9e949729627
chumpon-asaneerat <chumponsenate@gmail.com>
2024-02-19 04:55:49 +0700

    M3 Cord Production System #968 - Implements Wet Pick up #9

--- 02.Models/M3.Cord.Models/Models/DIP/S8x2WetPickUpItem.cs
+++ 02.Models/M3.Cord.Models/Models/DIP/S8x2WetPickUpItem.cs
@@ -49,6 +49,121 @@ namespace M3.Cord.Models
 
         #region Static Methods
 
+        public static NDbResult<List<S8x2WetPickUpItem>> Gets(string ProductCode, string LotNo, DateTime? DoffingDate)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<S8x2WetPickUpItem>> ret = new NDbResult<List<S8x2WetPickUpItem>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ProductCode", ProductCode);
+            p.Add("@LotNo", LotNo);
+            p.Add("@DoffingDate", DoffingDate);
+
+            try
+            {
+                var item = cnn.Query<S8x2WetPickUpItem>("GetS8x2WetPickUpItems", p,
+                    commandType: CommandType.StoredProcedure).ToList();
+                var data = item;
+                ret.Success(data);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Save
+        /// </summary>
+        /// <param name="value">The S8WetPickUp item to save.</param>
+        /// <returns></returns>
+        public static NDbResult<S8x2WetPickUpItem> Save(S8x2WetPickUpItem value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<S8x2WetPickUpItem> ret = new NDbResult<S8x2WetPickUpItem>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ProductCode", value.ProductCode);
+            p.Add("@LotNo", value.LotNo);
+            p.Add("@DoffingDate", value.DoffingDate);
+
+            p.Add("@DoffingNo", value.DoffingNo);
+            p.Add("@TwistNo", value.TwistNo);
+            p.Add("@RowType", value.RowType);
+            p.Add("@NipPressure", value.NipPressure);
+            p.Add("@WPU", value.WPU);
+            p.Add("@WPUErr", value.WPUErr);
+            p.Add("@WPUValue", value.WPUValue);
+
+            p.Add("@FirstAmt", value.FirstAmt);
+            p.Add("@UseAmt", value.UseAmt);
+            p.Add("@RestAmt", value.RestAmt);
+            p.Add("@ThrowAmt", value.ThrowAmt);
+
+            p.Add("@Operator", value.Operator);
+            p.Add("@UpdateDate", value.UpdateDate);
+            p.Add("@Remark", value.Remark);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveS8x2WetPickupItem", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
         #endregion
     }
 }
