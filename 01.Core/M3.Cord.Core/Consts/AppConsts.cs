8cace8de05bbe6bc1b54fd232051efb87de623ef
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-23 09:30:09 +0700

    M3 Cord Production System #1057 - Update App build 1526   - Sync code from kik.

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "1525";
-                    public static DateTime LastUpdate = new DateTime(2024, 04, 21, 03, 00, 00);
+                    public static string Build = "1526";
+                    public static DateTime LastUpdate = new DateTime(2024, 04, 23, 09, 30, 00);
                 }
 
                 public static class QA
