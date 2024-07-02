bdebfbebf98da999773a9a53bfe9af24b6b92ade
chumpon-asaneerat <chumponsenate@gmail.com>
2024-07-03 02:51:55 +0700

    M3 Cord Production System #1066 - Update app build 1066

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "1526";
-                    public static DateTime LastUpdate = new DateTime(2024, 04, 23, 09, 30, 00);
+                    public static string Build = "1066";
+                    public static DateTime LastUpdate = new DateTime(2024, 07, 03, 03, 00, 00);
                 }
 
                 public static class QA
