bd028c645d233fb3ad6ea4d0dd72e052a733152b
vorakanok <vorakanok@yahoo.com>
2024-08-02 17:28:37 +0700

    edit PrintZebraQRCode

--- 05.Controls/M3.Cord.Controls/Windows/LabelCHS9/LabelCHS9SearchWindow.xaml.cs
+++ 05.Controls/M3.Cord.Controls/Windows/LabelCHS9/LabelCHS9SearchWindow.xaml.cs
@@ -378,32 +378,32 @@ namespace M3.Cord.Windows
                                 if (result.ProductCode1 == "P1100ZFT")
                                 {
                                     barcode1 = "^CFA,15"
-                                        + "^FO1,15^GB50,50,3^FS"
+                                        + "^FO20,5^GB50,50,3^FS"
                                         + "^CF0,24"
-                                        + "^FWR^FO175,15 ,70,60^FD" + result.ProductCode1 + "^FS"
+                                        + "^FWR^FO195,5 ,70,60^FD" + result.ProductCode1 + "^FS"
                                         + "^CF0,24"
-                                        + "^FWR^FO150,15 ,70,60^FDSAP Code:7 179 172 90^FS"    
+                                        + "^FWR^FO170,5 ,70,60^FDSAP Code:7 179 172 90^FS"    
                                         + "^CF0,24"
-                                        + "^FWR^FO125,15 ,70,60^FD" + result.LotNoDoffNo1 + "^FS"
+                                        + "^FWR^FO145,5 ,70,60^FD" + result.LotNoDoffNo1 + "^FS"
                                         + "^CF0,24"
-                                        + "^FWR^FO100,15 ,70,60^FD" + "SP " + result.SPNo1.ToString() + "^FS"
+                                        + "^FWR^FO120,5 ,70,60^FD" + "SP " + result.SPNo1.ToString() + "^FS"
                                         + "^CFA,15"
-                                        + "^FO1,100"
+                                        + "^FO20,90"
                                         + "^BQ,2,5"
                                         + "^FDQA " + result.BarcodeText1 + "^FS";
                                 }
                                 else
                                 {
                                     barcode1 = "^CFA,15"
-                                       + "^FO1,15^GB50,50,3^FS"
+                                       + "^FO20,5^GB50,50,3^FS"
                                        + "^CF0,24"
-                                       + "^FWR^FO150,15 ,70,60^FD" + result.ProductCode1 + "^FS"
+                                       + "^FWR^FO170,5 ,70,60^FD" + result.ProductCode1 + "^FS"
                                        + "^CF0,24"
-                                       + "^FWR^FO125,15 ,70,60^FD" + result.LotNoDoffNo1 + "^FS"
+                                       + "^FWR^FO145,5 ,70,60^FD" + result.LotNoDoffNo1 + "^FS"
                                        + "^CF0,24"
-                                       + "^FWR^FO100,15 ,70,60^FD" + "SP " + result.SPNo1.ToString() + "^FS"
+                                       + "^FWR^FO120,5 ,70,60^FD" + "SP " + result.SPNo1.ToString() + "^FS"
                                        + "^CFA,15"
-                                       + "^FO1,100"
+                                       + "^FO20,90"
                                        + "^BQ,2,5"
                                        + "^FDQA " + result.BarcodeText1 + "^FS";
                                 }
@@ -429,32 +429,32 @@ namespace M3.Cord.Windows
                                 if (result.ProductCode2 == "P1100ZFT")
                                 {
                                     barcode2 = "^CFA,15"
-                                    + "^FO250,15^GB50,50,3^FS"
+                                    + "^FO290,5^GB50,50,3^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO425,15 ,70,60^FD" + result.ProductCode2 + "^FS"
+                                    + "^FWR^FO465,5 ,70,60^FD" + result.ProductCode2 + "^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO400,15 ,70,60^FDSAP Code:7 179 172 90^FS"
+                                    + "^FWR^FO440,5 ,70,60^FDSAP Code:7 179 172 90^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO375,15 ,70,60^FD" + result.LotNoDoffNo2 + "^FS"
+                                    + "^FWR^FO415,5 ,70,60^FD" + result.LotNoDoffNo2 + "^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO350,15 ,70,60^FD" + "SP " + result.SPNo2.ToString() + "^FS"
+                                    + "^FWR^FO390,5 ,70,60^FD" + "SP " + result.SPNo2.ToString() + "^FS"
                                     + "^CFA,15"
-                                    + "^FO250,100"
+                                    + "^FO290,90"
                                     + "^BQ,2,5"
                                     + "^FDQA " + result.BarcodeText2 + "^FS";
                                 }
                                 else
                                 {
                                     barcode2 = "^CFA,15"
-                                    + "^FO250,15^GB50,50,3^FS"
+                                    + "^FO290,5^GB50,50,3^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO400,15 ,70,60^FD" + result.ProductCode2 + "^FS"
+                                    + "^FWR^FO440,5 ,70,60^FD" + result.ProductCode2 + "^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO375,15 ,70,60^FD" + result.LotNoDoffNo2 + "^FS"
+                                    + "^FWR^FO415,5 ,70,60^FD" + result.LotNoDoffNo2 + "^FS"
                                     + "^CF0,24"
-                                    + "^FWR^FO350,15 ,70,60^FD" + "SP " + result.SPNo2.ToString() + "^FS"
+                                    + "^FWR^FO390,5 ,70,60^FD" + "SP " + result.SPNo2.ToString() + "^FS"
                                     + "^CFA,15"
-                                    + "^FO250,100"
+                                    + "^FO290,90"
                                     + "^BQ,2,5"
                                     + "^FDQA " + result.BarcodeText2 + "^FS";
                                 }
