ef5a65a68b18ce61423412ab03cc012043befc7c
chumpon-asaneerat <chumponsenate@gmail.com>
2024-10-15 08:00:15 +0700

    M3 Cord Production System #1150 - Update app build 1710

--- 01.Core/M3.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "1695";
-                    public static DateTime LastUpdate = new DateTime(2024, 09, 19, 13, 00, 00);
+                    public static string Build = "1710";
+                    public static DateTime LastUpdate = new DateTime(2024, 10, 15, 08, 10, 00);
                 }
 
                 public static class QA
